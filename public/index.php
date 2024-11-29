59f08b50578d9106a2af58a5777f247762ddd79d
23jammy <23jammycruz@gmail.com>
2024-11-29 12:16:16 +0800

    Modified log in layout

--- public/index.php
+++ public/index.php
@@ -23,8 +23,8 @@ if (isset($_SESSION['logged_in'])) {
     <script src="assets/js/jquery-3.7.1.min.js"></script>
     <title>Sign In</title>
 </head>
-<body class="flex w-screen min-h-screen bg-gradient-to-tr from-[#ff8989] via-[#f8b9b9] to-white">
-    <div class="flex flex-col w-full h-screen md:h-3/4 md:m-auto md:bg-transparent bg-[#f8b9b9]">
+<body class="flex w-screen min-h-screen bg-zinc-700">
+    <div class="flex flex-col w-full h-screen md:h-3/4 md:m-auto md:bg-transparent bg-zinc-700">
         <div class="flex items-center justify-center w-11/12 h-full p-5 mx-auto md:w-1/2">
             <div class="flex flex-col w-full p-4 bg-white shadow md:w-1/2 md:min-w-[30rem] rounded-xl h-auto">
                 <img class="w-40 h-40 m-auto md:mb-5 md:w-60 md:h-60 " src="assets/images/logo.png">
@@ -38,10 +38,10 @@ if (isset($_SESSION['logged_in'])) {
                         <i class="mx-2 text-xl text-gray-600 fa-solid fa-lock"></i>
                         <input id="password" name="password" type="password" class="w-10/12 h-full px-1 text-lg focus:outline-none" placeholder="Enter your password">
                     </div>
-                    <button id="logInBtn" type="submit"  class="w-40 h-10 p-1 mx-auto mt-10 text-xl font-semibold text-center text-white bg-gray-700 rounded-full hover:bg-gray-600">LOG IN</button>
+                    <div class="flex justify-end w-full h-10 p-1 mt-1 text-center text-black rounded-full"><a class="mr-12 text-orange-400 hover:underline" href="###">Forgot Password?</a></div>
+                    <button id="logInBtn" type="submit"  class="w-40 h-10 p-1 mx-auto mt-8 text-xl font-semibold text-center text-white bg-orange-600 rounded-full hover:bg-orange-500">LOG IN</button>
                 </form>
-                <div class="w-40 h-10 p-1 mx-auto mt-4 text-xl font-semibold text-center text-gray-700 rounded-full"><a class="hover:text-[#ff8989]" href="register.php">REGISTER</a></div>
-                <div class="w-40 h-10 p-1 mx-auto mt-5 text-center text-black rounded-full"><a class="hover:text-[#fc7474] hover:underline" href="###">Forgot password?</a></div>
+                <div class="h-10 p-1 mx-auto mt-5 text-center text-black rounded-full w-fit">Don't have an account? <a class="hover:text-orange-500 hover:underline" href="###">Register</a></div>
             </div>
         </div>
     </div>
