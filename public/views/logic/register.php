4ffba60072716734191cc92fed238f8eb89e4152
23jammy <23jammycruz@gmail.com>
2024-12-01 01:33:56 +0800

    Added registration backend

--- public/views/logic/register.php
+++ public/views/logic/register.php
@@ -1,18 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../../assets/css/output.css" rel="stylesheet">
-    <link href="../../../assets/css/fontawesome/all.min.css" rel="stylesheet" >
-    <link href="../../../assets/css/fontawesome/fontawesome.min.css" rel="stylesheet">
-    <script src="../../../assets/js/jquery-3.7.1.min.js"></script>
-    <title>Register</title>
-</head>
-<body class="flex w-screen min-h-screen bg-gray-400">
-    <div class="w-3/4 p-2 bg-white rounded-lg shadow h-fit">
-        <div>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+include_once "../../connect_db.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["reg_email"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $organization = $_POST["organization"];
+    $password = $_POST["set_password"];
+
+    $stmt = $mysqli->prepare("INSERT INTO users (email, first_name, last_name, organization, user_type, password)
+                            VALUES (?, ?, ?, ?, 1, SHA2(?, 256))");
+    $stmt->bind_param('sssis', $email, $first_name, $last_name, $organization, $password);
+    if ($stmt->execute()) {
+        echo "Account created successfully";
+    } else {
+        echo "Account cannot be created";
+    }
+    header("Location: ../../index.php");
+    $stmt->close();
+}
