b051df3d8d5a7ceb47155463fe08c151bda46ff9
23jammy <23jammycruz@gmail.com>
2024-12-07 09:48:15 +0800

    Image upload (#3)
    * Modified img display and buttons on venue grid
    
    * Adjusted compression

--- public/views/logic/create.php
+++ public/views/logic/create.php
@@ -5,18 +5,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $capacity_pax = $_POST['capacity_pax'];
     $description = $_POST['description'];
-    $image = $_POST['image'];
+    $image = $_FILES['image'];
 
-    $sql = "INSERT INTO venues (name, capacity_pax, description, image) VALUES (:name, :capacity_pax, :description, :image)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':capacity_pax', $capacity_pax);
-    $stmt->bindParam(':description', $description);
-    $stmt->bindParam(':image', $image);
-    $stmt->execute();
+    if(!empty($image['tmp_name'])) {
+        $source = $image['tmp_name'];
+        list($width, $height) = getimagesize($source);
+        $max_w = 896;
+        $max_h = 640;
+        $resize_ratio = min($max_w / $width, $max_h / $height);
+        $new_width = $width * $resize_ratio;
+        $new_height = $height * $resize_ratio;
 
-    header("Location: ../dashboard.php");
+        $info = getimagesize($source);
+        if ($info['mime'] == 'image/jpeg') {
+            $image = imagecreatefromjpeg($source);
+        } elseif ($info['mime'] == 'image/png') {
+            $image = imagecreatefrompng($source);
+        }
+
+        $tn = imagecreatetruecolor($new_width, $new_height);
+        imagecopyresampled($tn, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+        ob_start();
+        imagejpeg($tn, NULL, 60);
+        $img_content = ob_get_clean();
+
+        $sql = "INSERT INTO venues (name, capacity_pax, description, image) VALUES (:name, :capacity_pax, :description, :image)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':capacity_pax', $capacity_pax);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':image', $img_content, PDO::PARAM_LOB);
+        $stmt->execute();
+
+        header("Location: ../dashboard.php");
+    } else {
+        $sql = "INSERT INTO venues (name, capacity_pax, description) VALUES (:name, :capacity_pax, :description)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':capacity_pax', $capacity_pax);
+        $stmt->bindParam(':description', $description);
+        $stmt->execute();
+
+        header("Location: ../dashboard.php");
+    }
+
+
     exit();
 }
-?>
-
--- public/views/logic/update.php
+++ public/views/logic/update.php
@@ -14,14 +14,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $capacity_pax = $_POST['capacity_pax'];
     $description = $_POST['description'];
-    $image = $_POST['image'];
+    $image = $_FILES['image'];
+    $cur_image = $_POST['currentImg'];
+
+    if (!empty($image['tmp_name'])) {
+        $source = $image['tmp_name'];
+        list($width, $height) = getimagesize($source);
+        $max_w = 896;
+        $max_h = 640;
+        $resize_ratio = min($max_w / $width, $max_h / $height);
+        $new_width = $width * $resize_ratio;
+        $new_height = $height * $resize_ratio;
+
+        $info = getimagesize($source);
+        if ($info['mime'] == 'image/jpeg') {
+            $image = imagecreatefromjpeg($source);
+        } elseif ($info['mime'] == 'image/png') {
+            $image = imagecreatefrompng($source);
+        }
+
+        $tn = imagecreatetruecolor($new_width, $new_height);
+        imagecopyresampled($tn, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+        ob_start();
+        imagejpeg($tn, NULL, 60);
+        $img_content = ob_get_clean();
+    } else {
+        $img_content = base64_decode($cur_image);
+    }
 
     $sql = "UPDATE venues SET name = :name, capacity_pax = :capacity_pax, description = :description, image = :image WHERE idvenues = :idvenues";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':capacity_pax', $capacity_pax);
     $stmt->bindParam(':description', $description);
-    $stmt->bindParam(':image', $image);
+    $stmt->bindParam(':image', $img_content, PDO::PARAM_LOB);
     $stmt->bindParam(':idvenues', $id);
     $stmt->execute();
 
