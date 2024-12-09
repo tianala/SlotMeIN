c562dfef99b32b0ad9b77b9a092f0307c2517143
Christian Ala <202280241@psu.palawan.edu.ph>
2024-12-09 20:29:16 +0800

    add responsive lay-out

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,15 @@ module.exports = {
     "./public/layouts/*.{php,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   plugins: [],
-}
-
+};
